b0d02b33333d8bfaab4609023b77cc7622a941bc
Yudita_Khanodia <yudikhanodia2005@gmail.com>
2025-11-20 10:38:19 +0530

    WHERE in SQL

---
+++ Where.sql
@@ -0,0 +1,18 @@
+-- SELECT * FROM users 
+-- WHERE gender = "FEMALE";
+
+-- SELECT id,name FROM users
+-- WHERE gender <> "FEMALE";
+
+-- SELECT * FROM users
+-- WHERE id BETWEEN 10 AND 14;
+
+-- SELECT id FROM users 
+-- WHERE gender IN ("OTHERS");
+
+-- SELECT name FROM users 
+-- WHERE date_of_birth IS NOT NULL;
+
+-- SELECT name FROM users WHERE name LIKE 'A%'; (Name starts from A)
+-- SELECT name FROM users WHERE name LIKE '%b';  (Name ends with b)
+-- SELECT name FROM users WHERE name LIKE '%l%'; (Name contains l)
