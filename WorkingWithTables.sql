e2ac5d1ef7a0e49d0f8a0aa137cb6bcf18773e87
Yudita_Khanodia <yudikhanodia2005@gmail.com>
2025-11-20 10:29:20 +0530

    Added comments

--- WorkingWithTables.sql
+++ WorkingWithTables.sql
@@ -1,25 +1,25 @@
 -- USE startersql;
--- SELECT * FROM users;
--- SELECT name , gender , salary FROM users;
+SELECT * FROM users;
+SELECT name , gender , salary FROM users;
 
--- RENAME TABLE users TO customers;
+RENAME TABLE users TO customers;
 -- SELECT * FROM users;(Checking if there exist any user table after renaming)
 -- RENAME TABLE customers TO users;(Change to the name to the original one)
 
--- ALTER TABLE users
--- ADD COLUMN is_active BOOLEAN;
--- SELECT * FROM users;
+ALTER TABLE users
+ADD COLUMN is_active BOOLEAN;
+SELECT * FROM users;
 
--- ALTER TABLE users
--- DROP COLUMN is_active;
--- SELECT * FROM users;
+ALTER TABLE users
+DROP COLUMN is_active;
+SELECT * FROM users;
 
--- ALTER TABLE users
--- MODIFY COLUMN name VARCHAR(150);
+ALTER TABLE users
+MODIFY COLUMN name VARCHAR(150);
 
 
--- ALTER TABLE users
--- MODIFY COLUMN gender ENUM("MALE" , "FEMALE" , "OTHERS") AFTER name;
--- SELECT * FROM users;
+ALTER TABLE users
+MODIFY COLUMN gender ENUM("MALE" , "FEMALE" , "OTHERS") AFTER name;
+SELECT * FROM users;
 -- We can also move any column to the first
 -- ALTER TABLE users MODIFY COLUMN email VARCHAR(100) FIRST;
