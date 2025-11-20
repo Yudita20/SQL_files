3b3833ff0c21f0b4b425ceae99ee0b4e4d971e5f
Yudita_Khanodia <yudikhanodia2005@gmail.com>
2025-11-20 08:28:08 +0530

    Added comments

--- Creations(DataBase).sql
+++ Creations(DataBase).sql
@@ -1,4 +1,9 @@
 -- (Query for creating a database)Creation of database
 CREATE DATABASE startersql;
+
 -- (Query for using a database)USE data_basename;
 USE startersql;
+
+-- DROP Data_basename
+-- It will delete your database
+DROP startersql;
