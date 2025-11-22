adc20ec3ff4828f8b6562a919eaeb53bc22b3b73
Yudita_Khanodia <yudikhanodia2005@gmail.com>
2025-11-22 12:03:19 +0530

    Add the description

--- Update_in_SQL.sql
+++ Update_in_SQL.sql
@@ -1,22 +1,24 @@
--- USE startersql;
--- SELECT * FROM users;
+USE startersql;
+SELECT * FROM users;
 
--- UPDATE users
--- SET email = "aaravnew@example.com"
--- WHERE id = 1;
--- SELECT * FROM users;
+-- We can update single values
+UPDATE users
+SET email = "aaravnew@example.com"
+WHERE id = 1;
+SELECT * FROM users;
 
--- UPDATE users
--- SET email = "aarav@example.com" , salary = 68000
--- WHERE id = 1;
--- SELECT * FROM users;
+-- We can update mutiple values
+UPDATE users
+SET email = "aarav@example.com" , salary = 68000
+WHERE id = 1;
+SELECT * FROM users;
 
+SET AUTOCOMMIT = 0;
+UPDATE users
+SET salary = "68000";
+SELECT * FROM users;
+ROLLBACK;
+SELECT * FROM users;
+SET AUTOCOMMIT = 1;
 -- The updation of all values at once is not recommended.So,we use "where" to update the particular entity.
 -- But we can update the values at once when it is required.
--- SET AUTOCOMMIT = 0;
--- UPDATE users
--- SET salary = "68000";
--- SELECT * FROM users;
--- ROLLBACK;
--- SELECT * FROM users;
--- SET AUTOCOMMIT = 1;
