5b067a8613bce7cbb6ac7ef9963dc7f423a0f943
Yudita_Khanodia <yudikhanodia2005@gmail.com>
2025-11-24 16:28:04 +0530

    Add comments in Mathematical_functions in SQL

--- Mathematical_function.sql
+++ Mathematical_function.sql
@@ -1,14 +1,14 @@
--- SELECT * FROM users;
+SELECT * FROM users;
 
 -- ROUND , FLOOR , CEIL FUNCTION
--- SELECT 
--- ROUND(salary) AS rounded,
--- CEIL(salary) AS ceiled,
--- FLOOR(salary) AS floored
--- FROM users;
+SELECT 
+ROUND(salary) AS rounded,
+CEIL(salary) AS ceiled,
+FLOOR(salary) AS floored
+FROM users;
 
 -- MOD FUNCTION
--- SELECT id , MOD(id , 3) FROM users;
+SELECT id , MOD(id , 3) FROM users;
 
 
 
